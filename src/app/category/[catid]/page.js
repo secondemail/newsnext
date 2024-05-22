fad50adb2c1f1a0dfe332decd7d78b3eb49ea36e
secondemail <glost724@gmail.com>
2024-05-22 12:22:21 +0300

    ch ar ch cat

--- src/app/category/[catid]/page.js
+++ src/app/category/[catid]/page.js
@@ -5,41 +5,57 @@ import { PiArrowLineDownLeftBold } from 'react-icons/pi'
 import Image from 'next/image'
 import Link from 'next/link'
 import data from "../../../../public/api/db.json"
+import { RiEmotionSadLine } from "react-icons/ri";
 
 const OneCategory = ({params}) => {
   return (
     <Container>
-      <Row className='tw-py-10'>
-        {
-          data.category.filter(c=>c.id === params.catid )[0].subCat.map(i=>(
-            <Col key={i.id} lg="3" className='tw-pb-7 md:pb-0' >
-              <Link href={`/category/${params.catid}/subcat/${i.id}`}>
-                <h2 className="tw-text-[27px] tw-flex tw-pr-3 tw-gap-2 tw-items-center tw-font-bold tw-border-r-[10px] tw-border-[#07284a]">
-                  {i.title}
-                  <PiArrowLineDownLeftBold/>
-                </h2>
-              </Link>
-              <Row>
-                {
-                  data.news.filter(ar=>ar.subcatid === i.id).length > 0 ?
-                  data.news.filter(ar=>ar.subcatid === i.id).map(i=>(
-                      <Col className="hover:tw-bg-sky-900 hover:tw-text-white tw-transition" key={i} lg="12" xs="6">
-                        <Link href={`/articles/${i.id}`}>
-                          <div  className='tw-pt-7 md:tw-flex tw-items-center tw-gap-2 tw-border-b-2 tw-py-2'>
-                            <Image src={avatar} alt="as" width={130}/>
-                            <h1 className="tw-font-bold tw-text-[16px] tw-mb-2">
-                              {i.title}
-                            </h1>
-                          </div>
-                        </Link>
-                      </Col>
-                  )):(<h2>لايوجد مقالات</h2>)
-                }
-              </Row>
-            </Col>
-          ))
-        }
-      </Row>
+      {
+        data.category.filter(c=>c.id === params.catid ).length > 0 ?
+        (
+          <Row className='tw-py-10'>
+            {
+              data.category.filter(c=>c.id === params.catid )[0].subCat.map(i=>(
+                <Col key={i.id} lg="3" className='tw-pb-7 md:pb-0' >
+                  <Link href={`/category/${params.catid}/subcat/${i.id}`}>
+                    <h2 className="tw-text-[27px] tw-flex tw-pr-3 tw-gap-2 tw-items-center tw-font-bold tw-border-r-[10px] tw-border-[#07284a]">
+                      {i.title}
+                      <PiArrowLineDownLeftBold/>
+                    </h2>
+                  </Link>
+                  <Row>
+                    {
+                      data.news.filter(ar=>ar.subcatid === i.id).length > 0 ?
+                      data.news.filter(ar=>ar.subcatid === i.id).map(i=>(
+                          <Col className="hover:tw-bg-sky-900 hover:tw-text-white tw-transition" key={i} lg="12" xs="6">
+                            <Link href={`/articles/${i.id}`}>
+                              <div  className='tw-pt-7 md:tw-flex tw-items-center tw-gap-2 tw-border-b-2 tw-py-2'>
+                                <Image src={avatar} alt="as" width={130}/>
+                                <h1 className="tw-font-bold tw-text-[16px] tw-mb-2">
+                                  {i.title}
+                                </h1>
+                              </div>
+                            </Link>
+                          </Col>
+                      )):(<h2>لايوجد مقالات</h2>)
+                    }
+                  </Row>
+                </Col>
+              ))
+            }
+          </Row>
+        )
+        :
+        (
+          <div className='tw-text-center tw-pt-20'>
+            <div className="tw-flex tw-items-center tw-justify-center tw-text-[100px]">
+              4<RiEmotionSadLine/>4
+            </div>
+            <h2 className=" tw-text-[20px] tw-pb-6">عفوا هذه الصفحة غير موجود</h2>
+            <Link href="/" className=" tw-text-[20px] tw-text-sky-800">العوده للصفحة الرئيسية</Link>
+          </div>
+        )
+      }
     </Container>
   )
 }
--- src/app/category/[catid]/subcat/[subcatid]/page.js
+++ src/app/category/[catid]/subcat/[subcatid]/page.js
@@ -7,6 +7,7 @@ import { IoTimer } from "react-icons/io5";
 import { PiArrowLineDownLeftBold } from "react-icons/pi";
 import data from "../../../../../../public/api/db.json";
 import Link from "next/link";
+import { RiEmotionSadLine } from "react-icons/ri";
 
 
 
@@ -14,93 +15,110 @@ const SubCategory = ({params}) => {
 
   return (
     <Container>
-      <Row className='tw-py-10' >
-        <Col lg="7">
-          <Image src={avatar} alt='ss' className='tw-h-[300px]' />
-        </Col>
-        <Col lg="5" className='tw-flex tw-items-center'>
+      {
+        data.category.filter(c=>c.id === params.catid).length > 0 && data.news.filter(n=>n.subcatid===params.subcatid).length > 0 ?
+        (
           <div>
-            <h1 className='tw-font-bold tw-text-[40px]'>
-              {
-                data.category.filter(c=>c.id === params.catid)[0].subCat.filter(s=>s.id === params.subcatid)[0].title
-              }
-            </h1>
-            <p>
-              {
-                data.category.filter(c=>c.id === params.catid)[0].subCat.filter(s=>s.id === params.subcatid)[0].caption
-              }
-            </p>
-          </div>
-        </Col>
-      </Row>
-      <Row>
-        <Row>
-          <h2 className="tw-text-[30px] tw-flex tw-gap-4 tw-items-center tw-font-bold tw-border-r-[10px] tw-border-[#07284a]">
-            آخر الأخبار
-            <PiArrowLineDownLeftBold/>
-          </h2>
-        </Row>
-        <Row className="tw-pt-5 tw-flex tw-justify-center md:tw-justify-normal tw-gap-3">
-          {
-            data.news.filter(n=>n.subcatid===params.subcatid).length > 0 ?
-            data.news.filter(n=>n.subcatid===params.subcatid).map(i=>(
-              <Col key={i.id} lg="3" className="tw-border-2 hover:tw-animate-pulse tw-cursor-pointer tw-rounded-md tw-p-0 hover:tw-bg-[#07284a] hover:tw-text-white tw-transition">
-                <Link href={`/articles/${i.id}`}>
-                  <div>
-                    <Image className="tw-rounded-t-md tw-h-[200px]" src={avatar} alt="sm" />
-                  </div>
-                  <div className="tw-p-5">
-                    <span className="tw-text-[17px] tw-flex tw-pr-2 tw-mb-4 tw-font-bold tw-border-r-[5px] tw-border-[#07284a]">
-                      {i.cat}
-                    </span>
-                    <h1 className="tw-font-bold tw-text-[20px] tw-mb-2">
-                      {i.title}
-                    </h1>
-                    <span className="tw-flex tw-justify-end tw-gap-1 tw-items-center">
-                      <IoTimer className="tw-text-[20px] tw-text-gray-500 tw-mr-2" />
-                      10 دقيقة منذ الآن
-                    </span>
-                  </div>
-                </Link>
+            <Row className='tw-py-10' >
+              <Col lg="7">
+                <Image src={avatar} alt='ss' className='tw-h-[300px]' />
               </Col>
-            )):(<h2>لا يوجد مقالات</h2>)
-          }
-        </Row>
-      </Row>
-      <Row className='tw-pt-10'>
-        <Row>
-          <h2 className="tw-text-[30px] tw-flex tw-gap-4 tw-items-center tw-font-bold tw-border-r-[10px] tw-border-[#07284a]">
-            المزيد من الأخبار
-            <PiArrowLineDownLeftBold/>
-          </h2>
-        </Row>
-        <Row className="tw-pt-5 tw-flex tw-justify-center md:tw-justify-normal tw-gap-3">
-          {
-            data.news.filter(n=>n.subcatid===params.subcatid).length > 0 ?
-              data.news.filter(n=>n.subcatid===params.subcatid).map(i=>(
-                <Col key={i.id} lg="3" className="tw-border-2 hover:tw-animate-pulse tw-cursor-pointer tw-rounded-md tw-p-0 hover:tw-bg-[#07284a] hover:tw-text-white tw-transition">
-                  <Link href={`/articles/${i.id}`}>
-                    <div>
-                      <Image className="tw-rounded-t-md tw-h-[200px]" src={avatar} alt="sm" />
-                    </div>
-                    <div className="tw-p-5">
-                      <span className="tw-text-[17px] tw-flex tw-pr-2 tw-mb-4 tw-font-bold tw-border-r-[5px] tw-border-[#07284a]">
-                        {i.cat}
-                      </span>
-                      <h1 className="tw-font-bold tw-text-[20px] tw-mb-2">
-                        {i.title}
-                      </h1>
-                      <span className="tw-flex tw-justify-end tw-gap-1 tw-items-center">
-                        <IoTimer className="tw-text-[20px] tw-text-gray-500 tw-mr-2" />
-                        10 دقيقة منذ الآن
-                      </span>
-                    </div>
-                  </Link>
-                </Col>
-            )):(<h2>لا يوجد مقالات</h2>)
-          }
-        </Row>
-      </Row>
+              <Col lg="5" className='tw-flex tw-items-center'>
+                <div>
+                  <h1 className='tw-font-bold tw-text-[40px]'>
+                    {
+                      data.category.filter(c=>c.id === params.catid)[0].subCat.filter(s=>s.id === params.subcatid)[0].title
+                    }
+                  </h1>
+                  <p>
+                    {
+                      data.category.filter(c=>c.id === params.catid)[0].subCat.filter(s=>s.id === params.subcatid)[0].caption
+                    }
+                  </p>
+                </div>
+              </Col>
+            </Row>
+            <Row>
+              <Row>
+                <h2 className="tw-text-[30px] tw-flex tw-gap-4 tw-items-center tw-font-bold tw-border-r-[10px] tw-border-[#07284a]">
+                  آخر الأخبار
+                  <PiArrowLineDownLeftBold/>
+                </h2>
+              </Row>
+              <Row className="tw-pt-5 tw-flex tw-justify-center md:tw-justify-normal tw-gap-3">
+                {
+                  data.news.filter(n=>n.subcatid===params.subcatid).length > 0 ?
+                  data.news.filter(n=>n.subcatid===params.subcatid).map(i=>(
+                    <Col key={i.id} lg="3" className="tw-border-2 hover:tw-animate-pulse tw-cursor-pointer tw-rounded-md tw-p-0 hover:tw-bg-[#07284a] hover:tw-text-white tw-transition">
+                      <Link href={`/articles/${i.id}`}>
+                        <div>
+                          <Image className="tw-rounded-t-md tw-h-[200px]" src={avatar} alt="sm" />
+                        </div>
+                        <div className="tw-p-5">
+                          <span className="tw-text-[17px] tw-flex tw-pr-2 tw-mb-4 tw-font-bold tw-border-r-[5px] tw-border-[#07284a]">
+                            {i.cat}
+                          </span>
+                          <h1 className="tw-font-bold tw-text-[20px] tw-mb-2">
+                            {i.title}
+                          </h1>
+                          <span className="tw-flex tw-justify-end tw-gap-1 tw-items-center">
+                            <IoTimer className="tw-text-[20px] tw-text-gray-500 tw-mr-2" />
+                            10 دقيقة منذ الآن
+                          </span>
+                        </div>
+                      </Link>
+                    </Col>
+                  )):(<h2>لا يوجد مقالات</h2>)
+                }
+              </Row>
+            </Row>
+            <Row className='tw-pt-10'>
+              <Row>
+                <h2 className="tw-text-[30px] tw-flex tw-gap-4 tw-items-center tw-font-bold tw-border-r-[10px] tw-border-[#07284a]">
+                  المزيد من الأخبار
+                  <PiArrowLineDownLeftBold/>
+                </h2>
+              </Row>
+              <Row className="tw-pt-5 tw-flex tw-justify-center md:tw-justify-normal tw-gap-3">
+                {
+                  data.news.filter(n=>n.subcatid===params.subcatid).length > 0 ?
+                    data.news.filter(n=>n.subcatid===params.subcatid).map(i=>(
+                      <Col key={i.id} lg="3" className="tw-border-2 hover:tw-animate-pulse tw-cursor-pointer tw-rounded-md tw-p-0 hover:tw-bg-[#07284a] hover:tw-text-white tw-transition">
+                        <Link href={`/articles/${i.id}`}>
+                          <div>
+                            <Image className="tw-rounded-t-md tw-h-[200px]" src={avatar} alt="sm" />
+                          </div>
+                          <div className="tw-p-5">
+                            <span className="tw-text-[17px] tw-flex tw-pr-2 tw-mb-4 tw-font-bold tw-border-r-[5px] tw-border-[#07284a]">
+                              {i.cat}
+                            </span>
+                            <h1 className="tw-font-bold tw-text-[20px] tw-mb-2">
+                              {i.title}
+                            </h1>
+                            <span className="tw-flex tw-justify-end tw-gap-1 tw-items-center">
+                              <IoTimer className="tw-text-[20px] tw-text-gray-500 tw-mr-2" />
+                              10 دقيقة منذ الآن
+                            </span>
+                          </div>
+                        </Link>
+                      </Col>
+                  )):(<h2>لا يوجد مقالات</h2>)
+                }
+              </Row>
+            </Row>
+          </div>
+        )
+        :
+        (
+          <div className='tw-text-center tw-pt-20'>
+            <div className="tw-flex tw-items-center tw-justify-center tw-text-[100px]">
+              4<RiEmotionSadLine/>4
+            </div>
+            <h2 className=" tw-text-[20px] tw-pb-6">عفوا هذه الصفحة غير موجود</h2>
+            <Link href="/" className=" tw-text-[20px] tw-text-sky-800">العوده للصفحة الرئيسية</Link>
+          </div>
+        )
+      }
     </Container>
   )
 }
